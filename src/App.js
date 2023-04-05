945fe76826b5b1e3636f7da73f4c2bca1008ec4a
Sue Sui <suesui117@gmail.com>
2023-04-05 11:04:39 -0400

    should not work

--- src/App.js
+++ src/App.js
@@ -1,129 +1,129 @@
 
-//Reference https://github.com/remix-run/react-router/blob/dev/examples/basic/src/App.tsx
-//https://gist.github.com/mtlynch3/1b6b8d05032550465aa66bc0fa5da89e
+// //Reference https://github.com/remix-run/react-router/blob/dev/examples/basic/src/App.tsx
+// //https://gist.github.com/mtlynch3/1b6b8d05032550465aa66bc0fa5da89e
 
-import { Routes, Route, Link } from 'react-router-dom';
-import ApiData from './Users.js';
-import UserList from './UserList';
-import ColorList from './ColorList';
-
-function App() {
-
-  const users = [
-    { name: 'Alice', age: 25 },
-    { name: 'Bob', age: 30 },
-    { name: 'Charlie', age: 35 },
-  ];
-
-  const colors = ['red', 'green', 'blue', 'yellow'];
-
-  const image=""
-
-  return (
-    <div className="App" style={{
-      backgroundImage: `url(${image}})`,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-    }} >
-
-      hi
-      <Routes>
-        <Route index element={<Home />} />
-
-
-        <Route path="api-call" element={<ApiData />} />
-        <Route path="about" element={<About />} />
-        <Route path="youtube" element={<Youtube />} />
-
-        <Route path="/colors" element={<ColorList colors={colors} />} />
-
-        <Route path="/users" element={<UserList users={users} />} />
-
-      </Routes>
-    </div>
-  );
-}
-
-
-function Home() {
-  return (
-    <div style={{textAlign: 'center', color: 'white'}}>
-      <h2>Home</h2>
-      <Link to="/about" style={{color: 'white'}}>About page</Link>
-      <br></br>
-
-      <Link to="/api-call" style={{color: 'white'}}>api-call</Link>
-      <br></br>
-
-      <Link to="/youtube" style={{color: 'white'}}>Youtube page</Link>
-      <br></br>
-
-      <Link to="/users" style={{color: 'white'}}>users</Link>
-      <br></br>
-
-      <Link to="/colors" style={{color: 'white'}}>colors</Link>
-      <br></br>
-    </div>
-
-  );
-}
-
-
-
-function Youtube() {
-  return (
-    
-    <div>
-      <h2>youtube</h2>
-      <Link to="/">Home page</Link>
-      <br></br>
-      <a href='https://youtube.com' target='_blank' rel='noreferrer'>HAHAHAH
-        Click me -- YouTube</a>
-    </div>
-
-
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About me</h2>
-      <p> Spring 2023, Practical Web Development </p>
-      <Link to="/">Home page</Link>
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
-// import React from 'react';
+// import { Routes, Route, Link } from 'react-router-dom';
+// import ApiData from './Users.js';
 // import UserList from './UserList';
 // import ColorList from './ColorList';
 
 // function App() {
 
-
 //   const users = [
-//     { name: 'Alice', age: 25, sex: 'female' },
-//     { name: 'Bob', age: 30, sex: 'male' },
-//     { name: 'Charlie', age: 35, sex: 'male' },
+//     { name: 'Alice', age: 25 },
+//     { name: 'Bob', age: 30 },
+//     { name: 'Charlie', age: 35 },
 //   ];
 
-//   const colors = ['pink', 'yellow', 'blue'];
+//   const colors = ['red', 'green', 'blue', 'yellow'];
+
+//   const image=""
 
 //   return (
-//     <div className="App">
-//       <UserList users={users} />
-//       <ColorList colors={colors} />
+//     <div className="App" style={{
+//       backgroundImage: `url(${image}})`,
+//       backgroundRepeat: "no-repeat",
+//       backgroundSize: "cover",
+//       minHeight: "100vh",
+//       display: "flex",
+//       flexDirection: "column",
+//     }} >
+
+//       hi
+//       <Routes>
+//         <Route index element={<Home />} />
+
+
+//         <Route path="api-call" element={<ApiData />} />
+//         <Route path="about" element={<About />} />
+//         <Route path="youtube" element={<Youtube />} />
+
+//         <Route path="/colors" element={<ColorList colors={colors} />} />
+
+//         <Route path="/users" element={<UserList users={users} />} />
+
+//       </Routes>
+//     </div>
+//   );
+// }
+
+
+// function Home() {
+//   return (
+//     <div style={{textAlign: 'center', color: 'white'}}>
+//       <h2>Home</h2>
+//       <Link to="/about" style={{color: 'white'}}>About page</Link>
+//       <br></br>
+
+//       <Link to="/api-call" style={{color: 'white'}}>api-call</Link>
+//       <br></br>
+
+//       <Link to="/youtube" style={{color: 'white'}}>Youtube page</Link>
+//       <br></br>
+
+//       <Link to="/users" style={{color: 'white'}}>users</Link>
+//       <br></br>
+
+//       <Link to="/colors" style={{color: 'white'}}>colors</Link>
+//       <br></br>
+//     </div>
+
+//   );
+// }
+
+
+
+// function Youtube() {
+//   return (
+    
+//     <div>
+//       <h2>youtube</h2>
+//       <Link to="/">Home page</Link>
+//       <br></br>
+//       <a href='https://youtube.com' target='_blank' rel='noreferrer'>HAHAHAH
+//         Click me -- YouTube</a>
+//     </div>
+
+
+//   );
+// }
+
+// function About() {
+//   return (
+//     <div>
+//       <h2>About me</h2>
+//       <p> Spring 2023, Practical Web Development </p>
+//       <Link to="/">Home page</Link>
 //     </div>
 //   );
 // }
 
 // export default App;
+
+
+
+
+// // import React from 'react';
+// // import UserList from './UserList';
+// // import ColorList from './ColorList';
+
+// // function App() {
+
+
+// //   const users = [
+// //     { name: 'Alice', age: 25, sex: 'female' },
+// //     { name: 'Bob', age: 30, sex: 'male' },
+// //     { name: 'Charlie', age: 35, sex: 'male' },
+// //   ];
+
+// //   const colors = ['pink', 'yellow', 'blue'];
+
+// //   return (
+// //     <div className="App">
+// //       <UserList users={users} />
+// //       <ColorList colors={colors} />
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
